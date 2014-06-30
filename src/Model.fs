be835e110d2592973774441624869b3174add66c
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2014-06-30 01:41:21 -0400

    model production version

--- src/Model.fs
+++ src/Model.fs
@@ -21,6 +21,7 @@ module internal MethodInfo =
         | _ -> None
 
     let (|Abstract|_|) (m : MethodInfo) = if m.IsAbstract then Some() else None
+    let (|Virtual|_|) (m : MethodInfo) = if m.IsVirtual then Some() else None
 
 open MethodInfo
 
@@ -33,18 +34,19 @@ type Model() =
     let errorsChanged = Event<_,_>()
 
     static let proxyFactory = ProxyGenerator()
-    static let proxyOptions = ProxyGenerationOptions(hook = {
+    static let proxyOptions = ProxyGenerationOptions {
         new IProxyGenerationHook with
-            member __.ShouldInterceptMethod(_, methodInfo) = 
-                match methodInfo with 
-                | Abstract & PropertySetter _ | Abstract & PropertyGetter _ -> true 
+            member __.ShouldInterceptMethod(type', method') = 
+                match method' with 
+                | Virtual & PropertySetter propertyName when type'.GetProperty(propertyName).GetGetMethod() <> null -> true
+                | Virtual & PropertyGetter propertyName when type'.GetProperty(propertyName).GetSetMethod() <> null -> true 
                 | _ -> false 
             member __.NonProxyableMemberNotification(_, _) = ()          
             member __.MethodsInspected() = ()          
-    })
+    }
 
     static member Create<'T when 'T :> Model and 'T : not struct>([<ParamArray>] constructorArguments)  = 
-        let interceptors : IInterceptor[] = [| AbstractProperties() |]
+        let interceptors : IInterceptor[] = [| PropertyInterceptor() |]
         proxyFactory.CreateClassProxy(typeof<'T>, proxyOptions, constructorArguments, interceptors) |> unbox<'T>    
 
     interface INotifyPropertyChanged with
@@ -54,8 +56,10 @@ type Model() =
     member this.NotifyPropertyChanged propertyName = 
         propertyChangedEvent.Trigger(this, PropertyChangedEventArgs propertyName)
 
-    member this.NotifyPropertyChanged(PropertySelector property: Expr<'Model -> 'Property>) = 
-        this.NotifyPropertyChanged property.Name
+    member this.NotifyPropertyChanged(propertySelector: Expr)= 
+        match propertySelector with 
+        | Patterns.PropertyGet(_, property, _) -> this.NotifyPropertyChanged property.Name
+        | _ -> invalidOp "Expecting property getter expression"
 
     interface INotifyDataErrorInfo with
         member this.HasErrors = 
@@ -71,29 +75,38 @@ type Model() =
         errors.[propertyName] <- messages
         errorsChanged.Trigger(this, DataErrorsChangedEventArgs propertyName)
 
-and internal AbstractProperties() =
+and internal PropertyInterceptor() =
     let data = Dictionary()
 
     interface IInterceptor with
         member this.Intercept invocation = 
-            match invocation.Method, invocation.InvocationTarget with 
-                | Abstract & PropertySetter propertyName, (:? Model as model) -> 
-                    let _, value = data.TryGetValue propertyName
+            match invocation.Method with 
+                | Abstract & PropertySetter propertyName -> 
+                    let _, prevValue = data.TryGetValue propertyName
                     data.[propertyName] <- invocation.Arguments.[0]
-                    if invocation.Arguments.[0] <> value
+                    if invocation.Arguments.[0] <> prevValue
                     then 
+                        let model: Model = invocation.InvocationTarget |> unbox
                         model.NotifyPropertyChanged propertyName
                         model.SetErrors(propertyName, Array.empty)
 
-                | Abstract & PropertyGetter propertyName, _ ->
+                | Abstract & PropertyGetter propertyName ->
                     match data.TryGetValue propertyName with 
                     | true, value -> invocation.ReturnValue <- value 
                     | false, _ -> 
                         let returnType = invocation.Method.ReturnType
                         if returnType.IsValueType then 
                             invocation.ReturnValue <- Activator.CreateInstance returnType
-
-                | _ -> invocation.Proceed()
+                | Virtual & PropertySetter propertyName -> 
+                    let prevValue = invocation.TargetType.GetProperty(propertyName).GetValue(invocation.InvocationTarget)
+                    invocation.Proceed()
+                    if invocation.Arguments.[0] <> prevValue
+                    then 
+                        let model: Model = invocation.InvocationTarget |> unbox
+                        model.NotifyPropertyChanged propertyName
+                        model.SetErrors(propertyName, Array.empty)
+                | _ -> 
+                    invocation.Proceed()
 
 [<RequireQualifiedAccess>]
 module Mvc = 
