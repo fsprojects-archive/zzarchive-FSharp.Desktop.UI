2097f922762fb41c588ad2fc48840fb6982cc122
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2014-07-12 17:12:11 -0700

    Update generated documentation for version 0.0.0-beta

--- build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-.nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
-packages\FAKE\tools\FAKE.exe build.fsx %*
-pause
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-#workaround assembly resolution issues in build.fsx
-mono packages/FAKE/tools/FAKE.exe build.fsx $@
